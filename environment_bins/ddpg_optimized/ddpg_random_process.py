809d418ce438641a6d25d946657d097bacd91b03
aaalex42 <130674556+aaalex42@users.noreply.github.com>
2024-01-29 14:52:06 +0100

    added skistep environment

---
+++ environment_bins/ddpg_optimized/ddpg_random_process.py
@@ -0,0 +1,44 @@
+"""
+This code provides the possibility to add noise to actions,
+
+Reference: https://github.com/matthiasplappert/keras-rl/blob/master/rl/random.py
+"""
+
+import numpy as np
+
+
+class RandomProcess(object):
+    def reset_states(self):
+        pass
+
+
+class AnnealedGaussianProcess(RandomProcess):
+    def __init__(self, mu, sigma, sigma_min, n_steps_annealing) -> None:
+        self.mu = mu
+        self.sigma = sigma
+        self.n_steps = 0
+
+        if sigma_min is not None:
+            self.m = -float(sigma - sigma_min) / float(n_steps_annealing)
+            self.c = sigma
+            self.sigma_min = sigma_min
+        else:
+            self.m = 0.
+            self.c = sigma
+            self.sigma_min = sigma
+
+    @property
+    def current_sigma(self):
+        sigma = max(self.sigma_min, self.m * float(self.n_steps) + self.c)
+        return sigma
+
+
+class GaussianWhiteNoiseProcess(AnnealedGaussianProcess):
+    def __init__(self, mu=0., sigma=1.5, sigma_min=None, n_steps_annealing=1000, size=1):
+        super(GaussianWhiteNoiseProcess, self).__init__(mu=mu, sigma=sigma, sigma_min=sigma_min, n_steps_annealing=n_steps_annealing)
+        self.size = size
+
+    def sample(self):
+        sample = np.random.normal(self.mu, self.current_sigma, self.size)
+        self.n_steps += 1
+        return sample
