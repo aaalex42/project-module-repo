5ca24daa32c16803e8d3f1f3d7380985a47c2978
kilianhuemmer <kilian.huemmer@study.thws.de>
2024-02-06 22:31:14 +0100

    update

--- environment_vectorized_2products_dqn/main.py
+++ environment_vectorized_2products_dqn/main.py
@@ -9,9 +9,12 @@ def main():
 
     #print(env.action_space.shape[0])
 
-    agent = DQNAgent(skip_env, device='cuda', num_episodes=10, BATCH_SIZE=256, GAMMA=0.99, EPS_START=0.9, EPS_END=0.05, EPS_DECAY=1000, TAU=0.005, LR=1e-5)
+    agent = DQNAgent(skip_env, device='cuda', num_episodes=250, BATCH_SIZE=32, BATCH_START_SIZE=10000, MEMORY_SIZE=100000, GAMMA=0.99, EPS_START=0.9, EPS_END=0.02, EPS_DECAY=1000000, TAU=0.005, LR=1e-5)
     total_rewards, inventory_levels, _ = agent.train() #if num_episodes increases, simulation time has to be increased as well in psim_gym.py
     agent.plot_rewards(total_rewards, inventory_levels)
+    agent.plot_durations(show_result=True)
+    plt.ioff()
+    plt.show()
 
 
 if __name__ == "__main__":
